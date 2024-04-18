2f891ff725dbd722f246b2f971824120c23b6b42
FabianKel <86095196+FabianKel@users.noreply.github.com>
2024-04-17 20:07:41 -0600

    Update InsertarTablas.sql
    Edited for testing in server

--- APLL_Software/DB/InsertarTablas.sql
+++ APLL_Software/DB/InsertarTablas.sql
@@ -1,5 +1,5 @@
 -- Inserts Ana Paula
-INSERT INTO Detalles_Venta (Numero_Orden, RepuestoID, Cantidad, Rebaja, SubTotal)
+/*INSERT INTO Detalles_Venta (Numero_Orden, RepuestoID, Cantidad, Rebaja, SubTotal)
 VALUES (1, 101, 1, 0.20, 150.00),
        (1, 102, 2, 0.10, 250.00),
        (2, 103, 3, 0.20, 350.00);
@@ -10,4 +10,12 @@ VALUES (1, 101, 500.00, 001),
        (3, 103, 150.00, 003);
 
 INSERT INTO Repuestos (RepuestoID, CarroID, Nombre, Categoria, Precio_Unitario)
-VALUES (101, 1, 'Filtro de aceite', 'Filtros', 15.00);
+VALUES (101, 1, 'Filtro de aceite', 'Filtros', 15.00);*/
+
+--PRUEBA--
+
+INSERT INTO Carro (carroid, marca, linea, modelo, transmision)
+VALUES (1, 'Mazda', 'Protege', 1999, 'Automático');
+
+INSERT INTO Repuestos (RepuestoID, CarroID, Nombre, Categoria, Precio_Unitario)
+VALUES (1, 1, 'Filtro de aceite', 'Filtros', 15.00);
