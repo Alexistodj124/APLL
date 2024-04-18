287333fdb5dd9fdae5e2e70715d32e92c8c93981
FabianKel <drk.fab@gmail.com>
2024-04-17 19:52:03 -0600

    Edited CreacionDB file and created Inserts File

--- APLL_Software/DB/CreacionDB.sql
+++ APLL_Software/DB/CreacionDB.sql
@@ -168,17 +168,3 @@ CREATE TABLE Cambios_Salarios (
   Motivo VARCHAR(255),
   FOREIGN KEY (EmpleadoDPI) REFERENCES Empleados(EmpleadoDPI)
 );
-
--- Inserts Ana Paula
-INSERT INTO Detalles_Venta (Numero_Orden, RepuestoID, Cantidad, Rebaja, SubTotal)
-VALUES (1, 101, 1, 0.20, 150.00),
-       (1, 102, 2, 0.10, 250.00),
-       (2, 103, 3, 0.20, 350.00);
-
-INSERT INTO Compra_Repuesto (CompraRepuestoID, RepuestoID, Costo, ProveedorID)
-VALUES (1, 101, 500.00, 001),
-       (2, 102, 275.00, 002),
-       (3, 103, 150.00, 003);
-
-INSERT INTO Repuestos (RepuestoID, CarroID, Nombre, Categoria, Precio_Unitario)
-VALUES (101, 1, 'Filtro de aceite', 'Filtros', 15.00);
