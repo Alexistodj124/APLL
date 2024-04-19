3e6705d3f80fadee018d6fa0ffb6c9e309a0b5f0
Alexistodj124 <alexistodj@gmail.com>
2024-04-19 02:05:56 -0600

    Redirecciona por grupo
    Redirecciona al usuario a la pagina correcta al hacer login

--- APLL_Software/apll/views.py
+++ APLL_Software/apll/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Carro
-
+from django.shortcuts import redirect
 
 # Create your views here.
 
@@ -34,3 +34,22 @@ def filter_carros(request):
         'carros': queryset
     }
     return render(request, 'pruebarep.html', context)
+
+
+
+def redirect_by_group(request):
+    if request.user.is_authenticated:
+        user_groups = request.user.groups.all()
+        if user_groups.filter(name='Admin').exists():
+            # Redireccionar a Pagina ADMIN 
+            return redirect('url_admin')
+        elif user_groups.filter(name='Ventas').exists():
+            # Redireccionar a pagina Ventas
+            return redirect('url_ventas')
+        else:
+            # Redireciona a home
+            return redirect('url_home')
+    else:
+        # Redirecciona a LOGIN
+        return redirect('url_login')
+
