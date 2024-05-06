5d36d9a84cb0e6878fe4b6231b0e3da89b2ffe94
Alexistodj124 <alexistodj@gmail.com>
2024-05-06 11:24:41 -0600

    LOGIN Funcional

--- APLL_Software/apll/urls.py
+++ APLL_Software/apll/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 urlpatterns = [
     path('', views.filter_carros, name='home'),
-    path('login', views.login, name='login'),
+    path('login/', views.custom_login, name='login'),
     path('pruebarepuestos',views.filter_carros, name='pruebarepuestos'),
 
 ]
--- APLL_Software/apll/views.py
+++ APLL_Software/apll/views.py
@@ -1,15 +1,29 @@
 from django.shortcuts import render
 from .models import Carro
 from django.shortcuts import redirect
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 # Create your views here.
 
 def home(request):
     return render(request, 'home.html')
 
-def login(request):
+def custom_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            # Redirect to a success page or some other URL
+            return redirect('home')  # Adjust the URL name as needed
+        else:
+            # Return an error message if authentication fails
+            messages.error(request, 'Invalid username or password.')
+    # If the request method is not POST or authentication fails, render the login page
     return render(request, 'login.html')
-
 
 def filter_carros(request):
     marcas = Carro.objects.values_list('Marca', flat=True).distinct()
